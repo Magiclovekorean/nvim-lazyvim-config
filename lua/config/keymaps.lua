b0bf4ec175a2c67a23cdfa5d616db1d1b4a30e6c
Martí Forn <magiclovett@gmail.com>
2026-01-23 22:30:34 +0100

    Updated config by adding some plugins
    Added plugin/s and keymap/s

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Surroud with "" the selected text
+vim.keymap.set("x", '"', 'c"<ESC>pa"<ESC>')
